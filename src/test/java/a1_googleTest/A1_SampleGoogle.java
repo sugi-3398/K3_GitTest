d9cdd8c3e747fa0be928b78fec40b6fc4925473e
sugi-3398 <sugi.3398@gmail.com>
2023-09-13 20:14:22 +0530

    first commit

---
+++ src/test/java/a1_googleTest/A1_SampleGoogle.java
@@ -0,0 +1,41 @@
+package a1_googleTest;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class A1_SampleGoogle {
+
+	@Test
+	public void loginGoogleTest() throws IOException, InterruptedException {
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver wd = new ChromeDriver();
+		wd.manage().window().maximize();
+		wd.get("https://www.google.com/");
+		System.out.println(wd.getTitle());
+		WebElement element = wd.findElement(By.name("q"));
+		element.sendKeys("chennai");
+		element.submit();
+		System.out.println(wd.getTitle());
+		wd.navigate().back();
+		System.out.println(wd.getTitle());
+		wd.navigate().forward();
+		System.out.println(wd.getTitle());
+		Thread.sleep(2000);
+		File src=((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+		FileHandler.copy(src, new File("C:/vcentry coding/eclipse/K3_GitTest/screenshot/gooogle.png"));
+		wd.quit();
+
+	}
+}
